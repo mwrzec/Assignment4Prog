59fbc7a6a07006eb5964e1e25082e6ac479d6598
Marcel Wrzecionek <wrzeciom@sheridancollege.ca>
2025-04-11 13:04:51 -0400

    added a basic merge sort function

--- main.cpp
+++ main.cpp
@@ -33,6 +33,37 @@ void insertionSort(vector<int> arr) {
     }
 }
 
+int mergeCount = 0;
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    mergeCount++;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        mergeCount++;
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(vector<int> arr, int left, int right) {
+    if (left >= right) {
+        mergeCount++;
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
 int main() {
     vector<int> test = generateArray(10, true);
     insertionSort(test);
