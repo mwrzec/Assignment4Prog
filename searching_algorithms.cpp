c0d38191a12e9adc6b9957ea7965ec0537d72e21
Marcel Wrzecionek <wrzeciom@sheridancollege.ca>
2025-04-11 16:09:09 -0400

    added code to searching_algorithms.cpp file.

--- searching_algorithms.cpp
+++ searching_algorithms.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "searching_algorithms.h"
+
+int binarySearch(const vector<int>& arr, int key, int& count) {
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low <= high) {
+        count ++;
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) return mid;
+        if (arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
+    }
+
+    return -1;
+}
+
+int linearSearch(const vector<int>& arr, int key, int& count) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == key) return i;
+    }
+    return -1;
+}
