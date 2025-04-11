325b195882ca52bda15bbca3f30456e9b96275e1
Marcel Wrzecionek <wrzeciom@sheridancollege.ca>
2025-04-11 12:04:40 -0400

    added array generating function for sorted and reverse sorted arrays

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Starting Algorithm Analysis..." << endl;
+    return 0;
+}
+
+vector<int> generateArray(int size, bool reverseOrder) {
+    vector<int> arr(size);
+    for (int i = 0; i < n; i++) {
+        if (reverseOrder) {
+            arr[i] = size - i;
+        } else {
+            arr[i] = i;
+        }
+    }
+    return arr;
+}
